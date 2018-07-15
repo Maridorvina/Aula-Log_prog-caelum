96fe58687bcf943df77dd01df1ae782fbe13a006
Mariane Dorvina <mariane_frowz@hotmail.com>
2018-07-15 02:28:45 -0300

    Add aula-log-pg-caelum

---
+++ libRelogio.js
@@ -0,0 +1,10 @@
+//biblioteca e programa feito por Professor Lucas Takeshi(Caelum)
+function formataRelogio( tempoEmMinutos ){
+    var diasEmFloat = tempoEmMinutos / 1440
+    var dias = parseInt(diasEmFloat)
+    var minutosRestantes = tempoEmMinutos - (dias * 1440)
+    var tempoEmHorasFloat = minutosRestantes/60
+    var horas = parseInt(tempoEmHorasFloat)
+    var minutos = Math.ceil((tempoEmHorasFloat - horas) * 60)
+    return dias + " dia(s) " + horas.toString().padStart(2,0) + ":" + minutos.toString().padStart(2,0)
+}
---
+++ numero.js
@@ -0,0 +1,7 @@
+    //biblioteca e programa feito por Professor Lucas Takeshi 
+
+    var frase 
+	function pegarNumero(frase) {
+        parseFloat(prompt (frase).replace("." , ","))
+    
+    }
